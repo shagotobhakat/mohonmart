02cd95055ed5f20e989187b3680500477bc041de
shagotobhakat <shagoto@perfectfootwear.ltd>
2025-10-20 11:41:50 +0600

    added logo & letters inside two rows in loader screen

--- app/page.js
+++ app/page.js
@@ -1,25 +1,23 @@
-// import Banner from "@/components/banner";
-// import Proprietor from "@/components/proprietor";
-// import About from "@/components/about";
-// import CategoryItems from "@/components/categoryItems";
-// import Accessories from "@/components/accessories";
-// import Clothing from "@/components/clothing";
-// import Cosmetics from "@/components/cosmetics";
-// import ClientReview from "@/components/clientReview";
-import LoadingScreen from "@/components/loader";
+import Banner from "@/components/banner";
+import Proprietor from "@/components/proprietor";
+import About from "@/components/about";
+import CategoryItems from "@/components/categoryItems";
+import Accessories from "@/components/accessories";
+import Clothing from "@/components/clothing";
+import Cosmetics from "@/components/cosmetics";
+import ClientReview from "@/components/clientReview";
 
 export default function Home() {
   return (
     <>
-    {/* <Banner />
+    <Banner />
     <Proprietor />
     <About />
     <CategoryItems />
     <Accessories />
     <Clothing />
     <Cosmetics />
-    <ClientReview /> */}
-    <LoadingScreen/>
+    <ClientReview />
     </>
   );
 }
